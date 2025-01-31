0e11f05225e2a47dc85bb6be09caaa1d517e7904
Sourav <skverma10401@gmail.com>
2025-01-31 11:35:14 +0530

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'sourav','Get AWS Certified', CURRENT_DATE(), False);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'sourav','Get Azure Certified', CURRENT_DATE(), False);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'sourav','Get GCP Certified', CURRENT_DATE(), False);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'sourav','Get Devops Certified', CURRENT_DATE(), False);
---
+++ src/test/java/com/skverma/springboot/myfirstwebapp/MyfirstwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skverma.springboot.myfirstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
